aa7d5332f3460afea59c60c75027ca7ae6f67312
Jireh Battung <jirehgbattung@gmail.com>
2025-07-15 18:51:28 +0800

    Add tailark log in code

---
+++ src/components/icons.tsx
@@ -0,0 +1,7 @@
+import { IconLogin, IconLogout, IconCoinMoneroFilled } from '@tabler/icons-react';
+
+export const Icons = {
+  logOut: IconLogout,
+  logIn: IconLogin,
+  logo: IconCoinMoneroFilled,
+}
