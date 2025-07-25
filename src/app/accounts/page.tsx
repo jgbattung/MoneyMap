cbe9f6e6adf50ac7f5f9dd7d87b64a8eb6dcc97c
Jireh Battung <jirehgbattung@gmail.com>
2025-07-25 19:39:41 +0800

    Fix overlay issue

--- src/app/accounts/page.tsx
+++ src/app/accounts/page.tsx
@@ -6,7 +6,8 @@ import { Icons } from '@/components/icons'
 import CreateAccountDrawer from '@/components/forms/CreateAccountDrawer'
 
 const Accounts = () => {
-  const [createAccountOpen, setCreateAccountOpen] = useState(false);
+  const [createAccountSheetOpen, setCreateAccountSheetOpen] = useState(false);
+  const [createAccountDrawerOpen, setCreateAccountDrawerOpen] = useState(false);
 
   return (
     <div className="max-w-7xl mx-auto px-4 md:px-8 py-6">
@@ -14,28 +15,28 @@ const Accounts = () => {
         <h1 className='text-2xl font-semibold md:text-3xl lg:text-4xl md:font-bold'>Accounts</h1>
 
         <button 
-          onClick={() => setCreateAccountOpen(true)}
+          onClick={() => setCreateAccountSheetOpen(true)}
           className="hidden md:flex gap-2 items-center border rounded-md bg-secondary-600 hover:bg-secondary-700 px-4 py-2 text-base transition-all"
         >
           <Icons.createAccount size={20} />
           <span>Create account</span>
         </button>
         <CreateAccountSheet
-          open={createAccountOpen}
-          onOpenChange={setCreateAccountOpen}
+          open={createAccountSheetOpen}
+          onOpenChange={setCreateAccountSheetOpen}
           className="hidden md:block"
         />
 
         <button 
-          onClick={() => setCreateAccountOpen(true)}
+          onClick={() => setCreateAccountDrawerOpen(true)}
           className="hidden max-md:flex gap-2 items-center border rounded-md bg-secondary-600 hover:bg-secondary-700 px-4 py-2 text-sm transition-all"
         >
           <Icons.createAccount size={16} />
           <span>Create account</span>
         </button>
         <CreateAccountDrawer
-          open={createAccountOpen}
-          onOpenChange={setCreateAccountOpen}
+          open={createAccountDrawerOpen}
+          onOpenChange={setCreateAccountDrawerOpen}
           className="block md:hidden"
         />
       </div>
