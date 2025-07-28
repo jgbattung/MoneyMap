e3cabb1055c7e44f36777b79736f382b3b94284d
Jireh Battung <jirehgbattung@gmail.com>
2025-07-28 16:31:25 +0800

    Adjust grids

--- src/app/accounts/page.tsx
+++ src/app/accounts/page.tsx
@@ -46,7 +46,7 @@ const Accounts = () => {
         />
       </div>
 
-      <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 pt-20'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-10'>
         {accounts.map((account) => (
           <AccountCard
             key={account.id}
