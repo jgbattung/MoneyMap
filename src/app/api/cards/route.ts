0fa99b4d09a0d8e60b1f49ed8f480d81c6294d55
Jireh Battung <jirehgbattung@gmail.com>
2025-08-01 17:24:24 +0800

    Create GET and POST request for credit cards

---
+++ src/app/api/cards/route.ts
@@ -0,0 +1,86 @@
+import { auth } from "@/lib/auth";
+import { db } from "@/lib/prisma";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const session = await auth.api.getSession({
+      headers: await headers()
+    });
+
+    if (!session) {
+      return NextResponse.json(
+        { error: 'Unauthorized' },
+        { status: 401 }
+      );
+    }
+
+    const cards = await db.financialAccount.findMany({
+      where: {
+        userId: session.user.id,
+        accountType: 'CREDIT_CARD'
+      },
+      orderBy: {
+        currentBalance: 'desc'
+      },
+    });
+
+    return NextResponse.json(cards, { status: 200 });
+
+  } catch (error) {
+    console.error('Error getting accounts: ', error);
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const session = await auth.api.getSession({
+      headers: await headers()
+    });
+
+    if (!session) {
+      return NextResponse.json(
+        { error: 'Unauthorized' },
+        { status: 401 }
+      );
+    }
+
+    const body = await request.json();
+
+    const { name, initialBalance, addToNetWorth, statementDate, dueDate } = body;
+
+    if (!name ||  initialBalance === undefined) {
+      return NextResponse.json(
+        { error: 'Missing required fields: name, initialBalance' },
+        { status: 400 }
+      );
+    }
+
+    const card = await db.financialAccount.create({
+      data: {
+        userId: session.user.id,
+        name,
+        accountType: 'CREDIT_CARD',
+        initialBalance: parseFloat(initialBalance),
+        currentBalance: parseFloat(initialBalance),
+        addToNetWorth: addToNetWorth ?? false,
+        statementDate: statementDate ? parseInt(statementDate) : null,
+        dueDate: dueDate ? parseInt(dueDate) : null,
+      },
+    });
+
+    return NextResponse.json(card, { status: 201 });
+
+  } catch (error) {
+    console.error('Error creating account', error);
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
