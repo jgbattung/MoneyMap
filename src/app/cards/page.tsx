fd3588df60673e24d97f5b8157679908609554d0
Jireh Battung <jirehgbattung@gmail.com>
2025-08-04 18:39:09 +0800

    Create edit card drawer

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -2,6 +2,7 @@
 
 import CreateCardDrawer from '@/components/forms/CreateCardDrawer';
 import CreateCardSheet from '@/components/forms/CreateCardSheet';
+import EditCardDrawer from '@/components/forms/EditCardDrawer';
 import { Icons } from '@/components/icons';
 import CreditCardCard from '@/components/shared/CreditCardCard';
 import SkeletonCardCard from '@/components/shared/SkeletonCardCard';
@@ -12,6 +13,19 @@ const Cards = () => {
   const { cards, isLoading, error, refetchCards } = useCards();
   const [createCardsSheetOpen, setCreateCardsSheetOpen] = useState(false);
   const [createCardsDrawerOpen, setCreateCardsDrawerOpen] = useState(false);
+  const [editCardSheetOpen, setEditCardSheetOpen] = useState(false);
+  const [editCardDrawerOpen, setEditCardDrawerOpen] = useState(false);
+  const [selectedCardId, setSelectedCardId] = useState<string>('');
+
+  const handleCardClick = (accountId: string) => {
+    setSelectedCardId(accountId);
+
+    if (window.innerWidth >= 768) {
+      setEditCardSheetOpen(true);
+    } else {
+      setEditCardDrawerOpen(true);
+    }
+  };
 
   return (
     <div className="h-dvh max-w-7xl mx-auto px-4 md:px-8 py-6 flex flex-col">
@@ -47,34 +61,42 @@ const Cards = () => {
         />
       </div>
 
+      <EditCardDrawer
+        open={editCardDrawerOpen}
+        onOpenChange={setEditCardDrawerOpen}
+        className='block md:hidden'
+        cardId={selectedCardId}
+        onCardUpdated={refetchCards}
+      />
 
-        {isLoading ? (
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10'>
-            {Array.from({ length: 3 }, (_, index) => (
-              <SkeletonCardCard key={index} />
-            ))}
-          </div>
-        ) : error? (
-          <div>
-            ERROR
-          </div>
-        ) : cards.length === 0 ? (
-          <div>
-            NO CARDS YET
-          </div>
-        ) : (
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10'>
-            {cards.map((card) => (
-              <CreditCardCard
-                key={card.name}
-                name={card.name}
-                statementDate={card.statementDate}
-                dueDate={card.dueDate}
-                currentBalance={card.currentBalance}
-              />
-            ))}
-          </div>
-        )}
+      {isLoading ? (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10'>
+          {Array.from({ length: 3 }, (_, index) => (
+            <SkeletonCardCard key={index} />
+          ))}
+        </div>
+      ) : error? (
+        <div>
+          ERROR
+        </div>
+      ) : cards.length === 0 ? (
+        <div>
+          NO CARDS YET
+        </div>
+      ) : (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10'>
+          {cards.map((card) => (
+            <CreditCardCard
+              key={card.name}
+              name={card.name}
+              statementDate={card.statementDate}
+              dueDate={card.dueDate}
+              currentBalance={card.currentBalance}
+              onClick={() => handleCardClick(card.id)}
+            />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
