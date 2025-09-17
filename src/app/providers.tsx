69f834ca40e161e79421f609e38cc7c4ab41a31f
Jireh Battung <jirehgbattung@gmail.com>
2025-09-17 14:43:34 +0800

    Create endpoints for income types

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
 interface ProvidersProps {
@@ -26,7 +25,7 @@ export default function Providers({ children }: ProvidersProps) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   )
 }
