f43b545c018622f9adc4edd1eb3f7119b1fdd411
Jireh Battung <jirehgbattung@gmail.com>
2025-12-12 21:33:50 +0800

    Create NetWorthSection component

---
+++ src/components/dashboard/NetWorthSection.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import NetWorthDisplay from './NetWorthDisplay'
+import NetWorthInsights from './NetWorthInsights'
+
+const NetWorthSection = () => {
+  return (
+    <section className='w-full'>
+      <div className='grid grid-cols-1 lg:grid-cols-5 gap-4'>
+        <div className='lg:col-span-3'>
+          <NetWorthDisplay />
+        </div>
+
+        <div className='lg:col-span-2'>
+          <NetWorthInsights />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default NetWorthSection
