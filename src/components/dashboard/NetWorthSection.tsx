596a78f2d7f20a5269d2d6d7d9df50c3d463ea80
Jireh Battung <jirehgbattung@gmail.com>
2025-12-13 17:12:58 +0800

    Add in dashboard

--- src/components/dashboard/NetWorthSection.tsx
+++ src/components/dashboard/NetWorthSection.tsx
@@ -5,7 +5,7 @@ import NetWorthInsights from './NetWorthInsights'
 const NetWorthSection = () => {
   return (
     <section className='w-full'>
-      <div className='grid grid-cols-1 lg:grid-cols-5 gap-4'>
+      <div className='grid grid-cols-1 lg:grid-cols-5'>
         <div className='lg:col-span-3'>
           <NetWorthDisplay />
         </div>
