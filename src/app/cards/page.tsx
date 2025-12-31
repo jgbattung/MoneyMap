88d42ddd7fe0782eb7ca0117dfc0a2ab5a6abda5
Jireh Battung <jirehgbattung@gmail.com>
2025-12-31 14:51:28 +0800

    center pages

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -30,7 +30,7 @@ const Cards = () => {
   };
 
   return (
-    <div className="max-w-7xl px-4 py-6 pb-20 md:pb-6 flex flex-col">
+    <div className="max-w-7xl mx-auto px-4 py-6 pb-20 md:pb-6 flex flex-col">
       <div className='flex items-center justify-between flex-wrap gap-4'>
         <h1 className='text-2xl font-semibold md:text-3xl lg:text-4xl md:font-bold'>Cards</h1>
       
