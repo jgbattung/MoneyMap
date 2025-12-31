7d54faa985f6249c96775eb3801b8fd67e985906
Jireh Battung <jirehgbattung@gmail.com>
2025-12-31 14:43:22 +0800

    adjust bottom padding

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Reports = () => {
   return (
-    <div className="h-dvh max-w-7xl mx-auto px-4 md:px-8 py-6 flex flex-col">
+    <div className="max-w-7xl px-4 py-6 pb-20 md:pb-6 flex flex-col">
       {/* Page Header */}
       <div className='flex items-center justify-between flex-wrap gap-4'>
         <h1 className='text-2xl font-semibold md:text-3xl lg:text-4xl md:font-bold'>Reports</h1>
